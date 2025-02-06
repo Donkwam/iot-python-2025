efd8daa943880579efb560a1fd0f0b8a000013a6
Woo Dong Kwan <ehdrhks0509@naver.com>
2025-02-06 17:22:23 +0900

    4일차 - 아, 피곤하다~~

--- day04/py06_exception.py
+++ day04/py06_exception.py
@@ -3,6 +3,7 @@
 # 예외처리
 ## 오류, Error, 실수, Fault,
 ## 1. Error(문법적 오류) - 코딩하다가 빨간색 밑줄생기는 거
+##    오류표시가 안나는 코딩을 잘못한 오류 포함(!) mul(7, 6) -> 42예상, 결과13
 ## 2. Exception(실행중 발생 예외) - 문법오류 수정 후 실행하다가 비정상 종료되는 거
 ## 파이썬은 Error도 **Error고 Exception도 **Error
 ## 에디터 상에 오류표시가 나면 Error
@@ -13,7 +14,7 @@ for i in numbers:
     pass
 
 def mul(a, b):
-    return a + b
+    return a * b
 
 def div(a, b):
     return a / b
@@ -24,7 +25,15 @@ while True:
     if op == 'q':   # 종료조건
         break
     elif op == '*': # 곱하기
-        x, y = input('곱할 수 입력 > ').split
+        x, y = input('곱할 수 입력 > ').split()
         x = int(x)
         y = int(y)
-        print(f'{x} * {y} = {x * y}')
+        print(f'{x} * {y} = {mul(x , y)}')
+
+    elif op == '/': # 나누기
+        x, y = input('나눌 수 입력 > ').split()
+        x = int(x)
+        y = int(y)
+        print(f'{x} / {y} = {div(x , y)}')
+    else:
+        print('정확한 입력 요망')
